11a7addf10fcc23a4f358a505dbca64d71f6b993
jewoos2921 <jewoos2921@gmail.com>
2021-12-06 14:12:33 +0900

    page 33

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use rltk::{Rltk, GameState, RGB};
 use std::cmp::{max, min};
 use specs::prelude::*;
+use specs::{AccessorCow, RunningTime};
 use specs_derive::Component;
-
-struct State {
-    ecs: World,
-}
 
 #[derive(Component)]
 struct Position {
@@ -20,11 +17,17 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
 
+struct State {
+    ecs: World,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+        self.run_system();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
         for (pos, render) in (&positions, &renderables).join() {
@@ -33,13 +36,36 @@ impl GameState for State {
     }
 }
 
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
+impl State {
+    fn run_system(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50().with_title("Roguelike Tutorial").build()?;
     let mut gs = State { ecs: World::new() };
+    
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+
     gs.ecs.create_entity().with(Position { x: 40, y: 25 })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
@@ -55,7 +81,7 @@ fn main() -> rltk::BError {
                 ,
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
-            }).build();
+            }).with(LeftMover {}).build();
     }
 
     rltk::main_loop(context, gs)
