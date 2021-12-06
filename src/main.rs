5d22290a33a43ddf2c7a5b382da2e8ced6c07a91
jewoos2921 <jewoos2921@gmail.com>
2021-12-06 14:01:40 +0900

    page 28

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,15 @@ struct Renderable {
 }
 
 
-impl Component for Position {
-    type Storage = VecStorage<Self>,
-}
-
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -40,6 +40,24 @@ fn main() -> rltk::BError {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.create_entity().with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        }).build();
+
+    for i in 0..10 {
+        gs.ecs.create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('$')
+                ,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            }).build();
+    }
+
     rltk::main_loop(context, gs)
 }
 
