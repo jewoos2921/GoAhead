b946b4c56a9e451c2dd2dd4ec443e19cef59caf9
jewoos2921 <jewoos2921@gmail.com>
2021-12-06 14:53:35 +0900

    page 52

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
-use specs_derive::Component;
+use specs_derive::*;
 
 #[derive(Component)]
 struct Position {
@@ -16,88 +16,17 @@ struct Renderable {
     bg: RGB,
 }
 
-#[derive(Component)]
-struct LeftMover {}
-
 #[derive(Component, Debug)]
 struct Player {}
-
-struct State {
-    ecs: World,
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // Player movement
-    match ctx.key {
-        None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
-}
-
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk) {
-        ctx.cls();
-
-        player_input(self, ctx);
-        self.run_system();
-
-        let positions = self.ecs.read_storage::<Position>();
-        let renderables = self.ecs.read_storage::<Renderable>();
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
-
-        for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79; }
-        }
-    }
-}
-
-
-impl State {
-    fn run_system(&mut self) {
-        let mut lw = LeftWalker {};
-        lw.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
-}
 
 #[derive(PartialEq, Copy, Clone)]
 enum TileType {
     Wall,
     Floor,
+}
+
+struct State {
+    ecs: World,
 }
 
 pub fn xy_idx(x: i32, y: i32) -> usize {
@@ -131,6 +60,35 @@ fn new_map() -> Vec<TileType> {
     map
 }
 
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
+
 fn draw_map(map: &[TileType], ctx: &mut Rltk) {
     let mut x = 0;
     let mut y = 0;
@@ -138,11 +96,15 @@ fn draw_map(map: &[TileType], ctx: &mut Rltk) {
         // Render a tile depending upon the tile type
         match tile {
             TileType::Floor => {
-                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.),
+                ctx.set(x, y,
+                        RGB::from_f32(0.5, 0.5, 0.5),
+                        RGB::from_f32(0., 0., 0.),
                         rltk::to_cp437('.'));
             }
             TileType::Wall => {
-                ctx.set(x, y, RGB::from_f32(0., 1., 0.), RGB::from_f32(0., 0., 0.),
+                ctx.set(x, y,
+                        RGB::from_f32(0., 1., 0.),
+                        RGB::from_f32(0., 0., 0.),
                         rltk::to_cp437('#'));
             }
         }
@@ -155,6 +117,32 @@ fn draw_map(map: &[TileType], ctx: &mut Rltk) {
     }
 }
 
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+        self.run_system();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+
+impl State {
+    fn run_system(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50().with_title("Roguelike Tutorial").build()?;
@@ -162,7 +150,6 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
 
     gs.ecs.insert(new_map());
@@ -174,16 +161,6 @@ fn main() -> rltk::BError {
             bg: RGB::named(rltk::BLACK),
         }).with(Player {}).build();
 
-    for i in 0..10 {
-        gs.ecs.create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('$')
-                ,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            }).with(LeftMover {}).build();
-    }
 
     rltk::main_loop(context, gs)
 }
