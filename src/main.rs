c973dc5e5b47e82e086e8ad1933ce33a97b1888b
jewoos2921 <jewoos2921@gmail.com>
2021-12-07 14:22:00 +0900

    page 78-2

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,50 @@
+use specs::prelude::*;
+
+use rltk::{Rltk, GameState, RGB};
+
 mod map;
 mod player;
 mod component;
 mod visibility_system;
+mod rect;
 
-use rltk::{Rltk, GameState, RGB, VirtualKeyCode, Point};
-use specs::prelude::*;
-use std::cmp::{max, min};
-use specs_derive::*;
-use component::Viewshed;
-use map::new_map_rooms_and_corridors;
+pub use component::*;
+pub use map::*;
+pub use player::*;
+pub use rect::Rect;
 use visibility_system::VisibilitySystem;
-use crate::map::{Map, TileType};
 
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
+
+pub struct State {
+    pub ecs: World,
 }
 
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
 
-#[derive(Component, Debug)]
-struct Player {}
-
-pub struct Rect {
-    pub x1: i32,
-    pub x2: i32,
-    pub y1: i32,
-    pub y2: i32,
-}
-
-impl Rect {
-    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Rect {
-        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
-    }
-    // Returns true if this overlaps with other
-    pub fn intersect(&self, other: &Rect) -> bool {
-        self.x1 <= other.x2 && self.x2 >= other.x1 && self.y1 <= other.y2 && self.y2 >= other.y1
-    }
-    pub fn center(&self) -> (i32, i32) {
-        ((self.x1 + self.x2) / 2, (self.y1 + self.y2) / 2)
+impl State {
+    fn run_system(&mut self) {
+        let mut vis = VisibilitySystem {};
+        vis.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
-struct State {
-    ecs: World,
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+        self.run_system();
+
+        draw_map(&self.ecs, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
 }
 
 
@@ -81,68 +76,6 @@ fn new_map() -> Vec<TileType> {
 }
 
 
-pub fn draw_map(ecs: &World,
-                ctx: &mut Rltk) {
-    let map = ecs.fetch::<Map>();
-
-    let mut x = 0;
-    let mut y = 0;
-
-    for (idx, tile) in map.tiles.iter().enumerate() {
-        // Render a tile depending upon the tile type
-        if map.revealed_tiles[idx] {
-            let glyph;
-            let mut fg;
-
-            match tile {
-                TileType::Floor => {
-                    glyph = rltk::to_cp437('.');
-                    fg = RGB::from_f32(0.0, 0.5, 0.5);
-                }
-                TileType::Wall => {
-                    glyph = rltk::to_cp437('#');
-                    fg = RGB::from_f32(0.0, 1., 0.);
-                }
-            }
-            if !map.visible_tiles[idx] { fg = fg.to_greyscale() }
-            ctx.set(x, y, fg, RGB::from_f32(0.0, 0., 0.), glyph);
-        }
-        // Move the coordinates
-        x += 1;
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-
-
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk) {
-        ctx.cls();
-
-        self.run_system();
-
-        let map = self.ecs.fetch::<Map>();
-        draw_map(&map, ctx);
-
-        let positions = self.ecs.read_storage::<Position>();
-        let renderables = self.ecs.read_storage::<Renderable>();
-
-        for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
-
-impl State {
-    fn run_system(&mut self) {
-        let mut vis = VisibilitySystem {};
-        vis.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
-}
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
--- src/map.rs
+++ src/map.rs
@@ -115,3 +115,39 @@ impl Algorithm2D for Map {
         Point::new(self.width, self.height)
     }
 }
+
+
+pub fn draw_map(ecs: &World,
+                ctx: &mut Rltk) {
+    let map = ecs.fetch::<Map>();
+
+    let mut x = 0;
+    let mut y = 0;
+
+    for (idx, tile) in map.tiles.iter().enumerate() {
+        // Render a tile depending upon the tile type
+        if map.revealed_tiles[idx] {
+            let glyph;
+            let mut fg;
+
+            match tile {
+                TileType::Floor => {
+                    glyph = rltk::to_cp437('.');
+                    fg = RGB::from_f32(0.0, 0.5, 0.5);
+                }
+                TileType::Wall => {
+                    glyph = rltk::to_cp437('#');
+                    fg = RGB::from_f32(0.0, 1., 0.);
+                }
+            }
+            if !map.visible_tiles[idx] { fg = fg.to_greyscale() }
+            ctx.set(x, y, fg, RGB::from_f32(0.0, 0., 0.), glyph);
+        }
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
