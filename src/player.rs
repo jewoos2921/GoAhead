a82a79253f1c220339ee1cc0a4c7c6cb74125d3d
jewoos2921 <jewoos2921@gmail.com>
2021-12-07 16:19:46 +0900

    page 90-2

--- src/player.rs
+++ src/player.rs
@@ -14,11 +14,14 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
                                      &mut positions,
                                      &mut viewsheds).join() {
         let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
+        if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
 
             viewshed.dirty = true;
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
         }
     }
 }
