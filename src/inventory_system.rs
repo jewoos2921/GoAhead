a795d23e0aee1a9f053265bf97e6160b22da1af0
jewoos2921 <jewoos2921@gmail.com>
2021-12-08 17:41:10 +0900

    page 143

--- src/inventory_system.rs
+++ src/inventory_system.rs
@@ -1,6 +1,6 @@
 use std::fmt::format;
 use specs::prelude::*;
-use super::{WantsToPickupItem, Name, InBackPack, Position, GameLog};
+use super::{WantsToPickupItem, Name, InBackPack, Position, GameLog, CombatStats, WantsToDrinkPotion, Potion};
 
 pub struct ItemCollectionSystem {}
 
@@ -36,3 +36,38 @@ impl<'a> System<'a> for ItemCollectionSystem {
     }
 }
 
+pub struct PotionUseSystem {}
+
+impl<'a> System<'a> for PotionUseSystem {
+    #[allow(clippy::type_complexity)]
+    type SystemData = (
+        ReadExpect<'a, Entity>,
+        WriteExpect<'a, GameLog>,
+        Entities<'a>,
+        WriteStorage<'a, WantsToDrinkPotion>,
+        ReadStorage<'a, Name>,
+        ReadStorage<'a, Potion>,
+        WriteStorage<'a, CombatStats>
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (player_entity, mut gamelog, entities, mut wants_drink, names, potions,
+            mut combat_stats) = data;
+
+        for (entity, drink, stats) in (&entities, &wants_drink, &mut combat_stats).join() {
+            let potion = potions.get(drink.potion);
+
+            match potion {
+                None => {}
+                Some(potion) => {
+                    stats.hp = i32::min(stats.max_hp, stats.hp + potion.heal_amount);
+                    if entity == *player_entity {
+                        gamelog.entries.push(format!("You drink the {}, healing {} hp",
+                                                     names.get(drink.potion).unwrap().name, potion.heal_amount));
+                    }
+                    entities.delete(drink.potion).expect("Delete failed");
+                }
+            }
+        }
+    }
+}
